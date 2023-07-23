56547dd25b0481c5ef164a477a817e21c284cb96
Diego Panizza <diego_panizza@hotmail.com>
2023-07-23 12:28:54 -0300

    GET product by id

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -26,7 +26,6 @@ public class Product implements Serializable {
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
 	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Double price;
