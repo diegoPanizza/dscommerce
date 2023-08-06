053bf7a9562026b75b1f254d246238a7a0241a70
Diego Panizza <diego_panizza@hotmail.com>
2023-08-06 20:08:57 -0300

    UserDetailService

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -17,22 +17,22 @@ public class Role implements GrantedAuthority{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
+	private Long id;
 	private String authority;
 	
 	public Role() {
 	}
 
-	public Role(String id, String authority) {
+	public Role(Long id, String authority) {
 		this.id = id;
 		this.authority = authority;
 	}
 
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
